27770b9cb2da89cd0acddd4feadf7d926f034856
yurii Petrychenko <misterborman@gmail.com>
2026-01-03 21:06:11 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
+    react(),
   ],
   server: {
     open: {
